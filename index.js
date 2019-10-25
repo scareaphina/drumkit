b942700143bfb3f824a2a2c6c96d0ab64996ce50
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-10-25 15:26:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-document.querySelector("button").addEventListener("click", handleClick);
 
-function handleClick() {
+numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+document.querySelectorAll("button")[i].addEventListener("click", function() {
   alert("I got clicked!");
-}
+});
 
+}
