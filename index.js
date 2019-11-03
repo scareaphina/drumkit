c6728ff282b2c56ec8c7e281ec6436cb80503cbc
Scareaphina <52181920+scareaphina@users.noreply.github.com>
2019-11-03 12:53:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -75,6 +75,40 @@ function buttonAnimation(currentKey) {
 
   activeButton.classList.add("pressed");
 
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+  
+}
+();
+      break;
+
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
 }
 ;
 
